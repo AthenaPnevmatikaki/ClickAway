bd71146d1b8db7eec933c9a9dd10161d1d82ba5a
Athena Pnevmatikaki <a.pnevmatikaki@gmail.com>
2021-03-07 21:18:41 +0200

    Fixed id counter

--- src/Order.java
+++ src/Order.java
@@ -1,10 +1,12 @@
 public class Order {
-    private static int id = 0;
+    private static int idCounter = 0;
+    public int id;
     private final String productCode;
 
     public Order(String productCode) {
         this.productCode = productCode;
-        id ++;
+        id = idCounter;
+        idCounter ++;
     }
 
     public String toString() {
